16f6d34b4b3a2c60d8dcd692099a54a31a48f858
jseanpatel <jseanpatel@gmail.com>
2018-07-14 23:02:48 -0700

    final commit

--- FlyRight/Miscellaneous/AppDelegate.swift
+++ FlyRight/Miscellaneous/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         FirebaseApp.configure()
         GADMobileAds.configure(withApplicationID: "pub-7204561255681761")
         return true
